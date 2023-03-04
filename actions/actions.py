fd14e7ee2b630857e0e1c9341b91378f41378a98
Riccardo Mancini <riccardomancini98@gmail.com>
2023-03-04 14:09:59 +0100

    working on exercise button and "scheda allenamento"

--- actions/actions.py
+++ actions/actions.py
@@ -10,7 +10,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import EventType, SlotSet
+from rasa_sdk.events import EventType, SlotSet, FollowupAction
 from rasa_sdk.types import DomainDict
 import random
 import pandas as pd
@@ -27,25 +27,12 @@ import requests
 import csv
 from os import path
 import warnings
+
 warnings.filterwarnings("ignore")
 
-
 df = pd.read_csv('./esercizi.csv', encoding='utf-8', sep=';')
-#print(df.head(), df.shape)
-
-# estraggo i sottodataframe
-schienadf = df.loc[1:6] 
-bracciadf = df.loc[7:13]
-gambedf = df.loc[14:20]
-bustodf = df.loc[21:28]
-
-# mi creo i dataframe con gli indici di colonna randomici
-
-schienadf.index=np.random.permutation(np.arange(len(schienadf)))
-bracciadf.index=np.random.permutation(np.arange(len(bracciadf)))
-gambedf.index=np.random.permutation(np.arange(len(gambedf)))
-bustodf.index=np.random.permutation(np.arange(len(bustodf)))
-
+remain = df
+print(df.head(), df.shape)
 
 
 def isfloat(num):
@@ -75,7 +62,6 @@ class ValidateBmiForm(FormValidationAction):
         else:
             dispatcher.utter_message(text=f"Sembrerebbe che tu non abbia inserito correttamente il tuo peso..")
             return {"Epeso": None}
-
 
     def validate_Ealtezza(
             self,
@@ -173,95 +159,111 @@ class ActionCreateScheda(Action):
 
         # TODO: Andare a generare la scheda per l'utente sulla base degli slot settati
 
+        es_name = ['Squat', 'Bench press', 'Deadlift', 'Shoulder press', 'Leg press', 'Lat pulldown', 'Barbell curl',
+                   'Dumbbell fly', 'Plank', 'Russian twist']
+        target = ['Gambe', 'Petto', 'Schiena', 'Spalle', 'Gambe', 'Schiena', 'Bicipiti', 'Petto', 'Core', 'Addominali']
+        tempo_recupero = [120, 90, 150, 60, 120, 90, 60, 60, 30, 30]
+        ripetizioni = [5, 8, 5, 10, 12, 10, 12, 8, 60, 40]
+        peso = [100, 80, 120, 50, 150, 80, 30, 10, 0, 0]
 
-        fig = ff.create_table(df[['es_name', 'target']])
+        # Creazione del DataFrame
+        df = pd.DataFrame(
+            {'Nome esercizi': es_name, 'Target': target, 'Ripetizioni': ripetizioni,
+             'Peso': peso, 'Tempo di recupero': tempo_recupero})
+        df = df.set_index('Nome esercizi')
+        # Visualizzazione del DataFrame
+        print(df)
+
+        fig = ff.create_table(df, index=True, index_title='ESERCIZI')
         fig.update_layout(autosize=True, title_text='Scheda esercizi', margin={'t': 40, 'b': 20})
         fig.write_image("scheda.png", scale=2)
 
         client = imgbbpy.SyncClient('b8e02f4fc7878ae94060c35ba45fa540')
         image = client.upload(file='scheda.png')
-        #print(image.url)
+        # print(image.url)
         os.remove('scheda.png')
 
-
         dispatcher.utter_message(
-                text=f"Scheda creata! Visualizzala al seguente link: {image.url}")
+            text=f"Scheda creata! Visualizzala al seguente link: {image.url}")
 
         return {}
+
+
+class ActionEsercizi(Action):
+    def name(self) -> Text:
+        return "action_esercizi"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> Any:
+
+        global remain
+        # genero in maniera randomica e unica gli esercizi del blocco scelto
+        # poi aggiorno gli esercizi che ancora non sono stati mostrati
+        exs = remain[remain["target"] == tracker.get_slot("Ecorpoallenamento")]
+        rand_samp = 5 if exs.shape[0] > 4 else exs.shape[0]
+        exs = exs.sample(rand_samp).drop_duplicates()
+        remain = pd.merge(remain, exs, indicator=True, how='outer').query('_merge=="left_only"') \
+            .drop('_merge', axis=1)
+
+        # generazione bottoni
+        dispatcher.utter_message(text="Clicca su uno degli esercizi per avere più informazioni.",
+                                 buttons=[{"title": str(es['es_name']), "payload": ""} for idx, es in exs.iterrows()])
+
+        return [SlotSet("Ecorpoallenamento", None)]
+
 
 class ValidateEserciziForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_esercizi_form"
-    
+
     def validate_Eesercizi(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-        ) -> Dict[Text, Any]:
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> Dict[Text, Any]:
         if tracker.get_intent_of_latest_message() == "affermazione":
             dispatcher.utter_message(
                 text="questa è la descrizione dell' esercizio"
             )
         return {"Eesercizi": True}
-    
+
+
 class ValidateCorpoAllenamentoForm(FormValidationAction):
     def name(self) -> Text:
         return "validate_corpoallenamento_form"
-    
+
     def validate_Ecorpoallenamento(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-        ) -> Dict[Text, any]:
-        # mi costruisco il dataset degli esercizi per le gambe 
-        #gambedf = df.loc[14:20]
-        #genero in maniera randomica e non ripetitiva gli indici del dataframe degli esercizi gambe 
-        gambedf.index=np.random.permutation(np.arange(len(gambedf)))
-        
-
-        
-        if slot_value == ("gambe"):
-
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> Dict[Text, any]:
+        global remain
+        if slot_value.capitalize() not in df['target'].unique():
             dispatcher.utter_message(
-                buttons= [
-                    {"title":str(gambedf.iloc[0]['es_name']) ,"payload":str(gambedf.iloc[0]['desc'])},
-                    {"title":str(gambedf.iloc[1]['es_name']) ,"payload":str(gambedf.iloc[1]['desc'])},
-                    {"title":str(gambedf.iloc[2]['es_name']) ,"payload":str(gambedf.iloc[2]['desc'])},
-                    {"title":str(gambedf.iloc[3]['es_name']) ,"payload":str(gambedf.iloc[3]['desc'])}
-                    ])
+                text=f"Non riconosco questa parte del corpo. I target gestiti sono: {'/'.join(df['target'].unique())}.")
+            return {"Ecorpoallenamento": None}
 
-
-        elif slot_value == ("braccia"):
-             dispatcher.utter_message(
-                text="questi sono gli esercizi delle braccia"
-            )
-        elif slot_value == ("schiena"):
-             dispatcher.utter_message(
-                text="questi sono gli esercizi delle schiena"
-            )
-        elif slot_value == ("busto"):
-             dispatcher.utter_message(
-                text="questi sono gli esercizi delle busto"
-            )
+        # check se sono già stati mostrati tutti gli esercizi disponibili
+        if remain[remain["target"] == slot_value.capitalize()].shape[0] == 0:
+            dispatcher.utter_message(
+                text="Ti ho già mostrato tutti gli esercizi che avevo disponibili, perciò te li ripropongo!")
+            remain = df
         else:
             dispatcher.utter_message(
-                text="la tua richiesta non è valida (grupby colonne dataframe)"
-            )
-        return {"Ecorpoallenamento" : slot_value }
+                text=f"OK! Di seguito troverai alcuni esercizi del blocco '{slot_value.capitalize()}'")
+        return {"Ecorpoallenamento": slot_value.capitalize()}
 
 
-
-
-# Azione per la creazinoe di pulsanti
 class AskEserciziInfo(Action):
     def name(self) -> Text:
         return "action_ask_Eesercizi"
+
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[EventType]:
-        
-        return[]
+        return []
